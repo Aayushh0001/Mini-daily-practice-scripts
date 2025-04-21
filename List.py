7e4983411f3e4cb98068616079e709c60063f491
Aayush <pantaayush234@gmail.com>
2025-04-21 20:04:55 +0545

    Lists and Collections

--- List.py
+++ List.py
@@ -1,7 +1,7 @@
 Fruits = ["apple","banana","cherry"]
-print(Fruits[0])
-Fruits.append("mango")
-Fruits.remove("banana")
+print(Fruits[0])#Indexing always starts at 0
+Fruits.append("mango")#This adds a value at the last of the List
+Fruits.remove("banana")#This removes the value 
 Fruits.append("banana")
-Fruits.extend("Pineapple")
+Fruits.extend("Pineapple")#This shows the value in each letter
 print(Fruits)

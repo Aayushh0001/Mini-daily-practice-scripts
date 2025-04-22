e0b895200515c2d67ea9869340e7d069e7f827ac
Aayush <pantaayush234@gmail.com>
2025-04-22 20:27:36 +0545

    Looping_Collections

---
+++ Collections_Looping.py
@@ -0,0 +1,16 @@
+#List
+Fruit = ["Apple","Banana"]
+for fruit in Fruit:
+    print(fruit)
+    
+#Tuple
+Coordinates=[4,5,6]
+for value in Coordinates:
+    print(value)
+
+#Dictionary
+Student = {
+    "Name":"Aayush"
+}
+for Key,value in Student.items():
+    print(Key,":",value)

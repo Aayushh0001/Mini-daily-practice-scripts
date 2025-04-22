3a3e4f735be22111b168af65fc889e4ab8e58705
Aayush <pantaayush234@gmail.com>
2025-04-22 20:35:28 +0545

    Book Shelf

---
+++ BookShelf.py
@@ -0,0 +1,10 @@
+books=[
+    {"title":"Atomic Habits","Author":"James Clear"},
+    {"title":"Harry Potter","Author":"JK Rowling"},
+    {"title":"Deep Work","Author":"Cal Newport"}
+]
+
+for book in books:
+    print("Title:",book["title"])
+    print("Author:",book["Author"])
+    print()

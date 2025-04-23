2282e4ead2be266c75d3dbd14206e6d7a51355a2
Aayush <pantaayush234@gmail.com>
2025-04-23 21:25:12 +0545

    Day 5

---
+++ Student.py
@@ -0,0 +1,9 @@
+Student = {
+    "Name" : "Aayush Pant",
+    "Age"  : 20,
+    "Email":"Pantaayush234@gmail.com"
+}
+
+print("Name:",Student["Name"])
+print("Age:",Student["Age"])
+print("Email:",Student["Email"])

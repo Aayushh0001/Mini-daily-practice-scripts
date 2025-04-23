6c1e1e458ca51a302cca85bdc406f14107308c8f
Aayush <pantaayush234@gmail.com>
2025-04-23 21:27:53 +0545

    Day 5

---
+++ Updated_Student.py
@@ -0,0 +1,12 @@
+Student = {
+    "Name" : "Aayush Pant",
+    "Age"  : 20,
+    "Email":"Pantaayush234@gmail.com"
+}
+
+Student["Graduated"]="False"
+
+print("Name:",Student["Name"])
+print("Age:",Student["Age"])
+print("Email:",Student["Email"])
+print(Student)

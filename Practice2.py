7b618ed52ebe04bcc4d1d87b82a18c5e022dbad2
Aayush <pantaayush234@gmail.com>
2025-04-24 21:26:55 +0545

    Practice 2

---
+++ Practice2.py
@@ -0,0 +1,11 @@
+def Square(a,b):
+    return a+b
+result = Square(2,3)
+Result= result*result
+print(Result)
+
+#MORE CLEANER VERSION:
+#def square_of_sum(a,b):
+#return (a+b) ** 2
+#result=square_of_sum(2,3)
+#print(result)

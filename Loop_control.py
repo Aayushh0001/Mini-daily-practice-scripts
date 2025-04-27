661bc6ce6a999bb11f0b77147140beca21130631
Aayush <pantaayush871@gmail.com>
2025-04-27 19:50:38 +0545

    Day 7

---
+++ Loop_control.py
@@ -0,0 +1,15 @@
+#Break
+for i in range(10):
+    if i==5:
+        break
+    print(i)
+
+#Continue
+for i in range(5):
+    if i==2:
+        continue
+    print(i)
+
+#Pass(PlaceHolder)
+for i in range(10):
+    pass

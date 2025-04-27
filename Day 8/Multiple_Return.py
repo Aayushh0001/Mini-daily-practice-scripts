3d31a8a2a60e72225a358474d2f7eaf43851d9d9
Aayush <pantaayush871@gmail.com>
2025-04-27 21:39:36 +0545

    Practice 1

---
+++ Day 8/Multiple_Return.py
@@ -0,0 +1,7 @@
+def Calculate(a,b):
+    return a+b,a-b,a*b
+sum_result,diff_result,mult_result=Calculate(26,9)
+print("Sum of a and b is ",sum_result)
+print("Difference of a and b is ",diff_result)
+print("Multiplication of a and b is ",mult_result)
+

8408f705aaef6b26a72dcb4d318d6a58cd209266
Aayush <pantaayush871@gmail.com>
2025-05-01 17:53:35 +0545

    New changes

---
+++ Day 9/Tuple_Practice.py
@@ -0,0 +1,5 @@
+Tuple=["apple","banana","cherry","grape","cherry"]
+index = Tuple.index("banana")
+count = Tuple.count("cherry")
+print("Index of banana is ", index)
+print("Count of cherry is",count)

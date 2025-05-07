aa34e4a49ab8adac7ba6de32b7fcf2662d268fe7
Aayush <pantaayush871@gmail.com>
2025-05-07 19:49:12 +0545

    Day 10

---
+++ Day 9/Mix.py
@@ -0,0 +1,6 @@
+students = [("Aayush",20),("Soumyika",21),("Ram",22)]
+
+second_student = students[1]
+print("Second student tuple:",second_student)
+
+print("Name of second student:",second_student[0])

1dfaa222d3fa47cc09cdc4a301e172f3213a2713
Aayush <pantaayush871@gmail.com>
2025-05-07 20:05:59 +0545

    Day 10

---
+++ advanced.py
@@ -0,0 +1,15 @@
+student={
+"name":"aayush",
+"Age":20,
+"language":"python"}
+
+print(student.get("name"))
+
+#Adding a new key
+student["college"]="NCIT"
+student.pop("Age")
+
+#Looping through keys and values
+for key,value in student.items():
+ print(key,"->",value)
+
